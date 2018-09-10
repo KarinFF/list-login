ca76f0ab9fe6b5caa94c887732a8c35fd82614e3
KarinFF <karinfrostell@gmail.com>
2018-09-10 14:04:48 +0200

    first commit
    starting up the project

---
+++ react-firebase/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+
+// Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDEjBBqWcIJwiE0cgsIx45ghnAUAQomRDI",
+    authDomain: "elk-login-4a498.firebaseapp.com",
+    databaseURL: "https://elk-login-4a498.firebaseio.com",
+    projectId: "elk-login-4a498",
+    storageBucket: "elk-login-4a498.appspot.com",
+    messagingSenderId: "630239457802"
+  };
+  firebase.initializeApp(config)
+
+export default firebase
